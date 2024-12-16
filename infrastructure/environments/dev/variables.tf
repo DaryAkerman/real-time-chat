f24bda4329da680bfcdcfff16737bee2e70a6c5e
DaryAkerman <daryakerman2002@gmail.com>
2024-12-16 14:19:47 +0200

    made db username a variable aswell

--- infrastructure/environments/dev/variables.tf
+++ infrastructure/environments/dev/variables.tf
@@ -1,4 +1,9 @@
 variable "admin_password" {
-  description   = "The password for the database admin user"
-  type          = string
+    description     = "The password for the database admin user"
+    type            = string
+}
+
+variable "admin_username" {
+    description     = "The username for the database admin user"
+    type            = string
 }
